e4ddca8003d32f3d8e98a60b3ffbc647c657f1cb
CemPesdereli <cempesdereli@gmail.com>
2024-07-29 19:55:58 +0300

    TopKFrequentElements method_2 added

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TopKFrequentElements {
 
@@ -35,11 +32,33 @@ public class TopKFrequentElements {
 
 
     }
+    public static int[] topKFrequent_2(int[] nums, int k) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        Queue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b)-> b.getValue() - a.getValue());
+
+        pq.addAll(map.entrySet());
+
+        int[] result = new int[k];
+        for(int i =0; i<k;i++){
+            result[i]=pq.poll().getKey();
+        }
+        return result;
+
+
+
+
+    }
 
     public static void main(String[] args) {
         int[] nums = new int[] {1,2,2,3,3,3};
         int k = 2;
-        int[] resultArray= topKFrequent(nums,k);
+        int[] resultArray= topKFrequent_2(nums,k);
         for (int a : resultArray){
             System.out.println(a);
         }
